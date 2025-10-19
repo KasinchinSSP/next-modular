55fb9727a07f0419c7525b5bd735800dcd3882f5
ShinOperation <kasinchin.shinoperation@gmail.com>
2025-10-19 19:44:42 +0700

    feat: define transpilePackages in Next.js configuration for module compatibility

--- apps/web-b/next.config.ts
+++ apps/web-b/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ["@pkg/ui", "@pkg/modules"],
 };
 
 export default nextConfig;
