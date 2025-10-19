3b0c5597c3e1df0ae2756a62725828fa2c96faf8
ShinOperation <kasinchin.shinoperation@gmail.com>
2025-10-19 16:57:51 +0700

    Add Tailwind CSS configuration and update tsconfig.json formatting

---
+++ apps/web-a/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "../../packages/**/*.{ts,tsx}",
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
